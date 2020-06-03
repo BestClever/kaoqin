cc965f545d9eec29559d03a1be67b8a0768fab7d
xiacong <xiacong@talkweb.com.cn>
2020-06-03 19:44:12 +0800

    修改bug

---
+++ src/main/java/com/kaoqin/util/Audience.java
@@ -0,0 +1,11 @@
+package com.kaoqin.util;
+
+/**
+ * @author BestClever
+ * @title: Audience
+ * @projectName kaoqin
+ * @description: TODO
+ * @date 2020-05-29 19:14
+ */
+public class Audience {
+}
